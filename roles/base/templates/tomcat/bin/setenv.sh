36154f7bf9c75d4c00cdfb819d5be337e792a359
dongxin <dongxin@3vjuyuan.com>
2017-07-25 13:24:23 +0800

    [TASK] Add java maven and tomcat

--- roles/base/templates/tomcat/bin/setenv.sh
+++ roles/base/templates/tomcat/bin/setenv.sh
@@ -4,15 +4,15 @@ LOGGING_CONFIG=-Dnop
 # logging
 JAVA_OPTS="$JAVA_OPTS -Djuli-logback.configurationFile=file://$CATALINA_HOME/conf/logback.xml"
 # memory settings
-JAVA_OPTS="$JAVA_OPTS -Xms{{min_heap_size}} -Xmx{{max_heap_size}}"
-JAVA_OPTS="$JAVA_OPTS -XX:MetaspaceSize={{metaspace_size}} -XX:MaxMetaspaceSize={{max_metaspace_size}}"
+JAVA_OPTS="$JAVA_OPTS -Xms{{tomcat.min_heap_size}} -Xmx{{tomcat.max_heap_size}}"
+JAVA_OPTS="$JAVA_OPTS -XX:MetaspaceSize={{tomcat.metaspace_size}} -XX:MaxMetaspaceSize={{tomcat.max_metaspace_size}}"
 # dump settings
-JAVA_OPTS="$JAVA_OPTS -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath={{heap_dump_dir}}"
+JAVA_OPTS="$JAVA_OPTS -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath={{tomcat.heap_dump_dir}}"
 # gc settings
 JAVA_OPTS="$JAVA_OPTS -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled"
-JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCDetails -XX:+PrintGCDateStamps -verbose:gc -Xloggc:{{gc_log_file}}"
+JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCDetails -XX:+PrintGCDateStamps -verbose:gc -Xloggc:{{tomcat.gc_log_file}}"
 JAVA_OPTS="$JAVA_OPTS -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=7 -XX:GCLogFileSize=10M"
-{% if ipv6_disable %}
+{% if tomcat.ipv6_disable %}
 # ipv6 disabled
 JAVA_OPTS="$JAVA_OPTS -Djava.net.preferIPv4Stack=true"
 {% endif %}
